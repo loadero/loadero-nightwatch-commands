2c84cae062b10cae4cd7da0d3a7ffade3f6418c4
Andris Papirats-Pfafrats <38584172+andris-p-pfafrats@users.noreply.github.com>
2024-06-28 10:16:29 +0300

    LOAD-3185: Add log message if no alert is open (#10)
    * LOAD-3185: Add log message if no alert is open
    
    * Apply suggestions from code review

--- nightwatch/commands/ignoreAlert.js
+++ nightwatch/commands/ignoreAlert.js
@@ -6,9 +6,14 @@ class IgnoreAlert extends EventEmitter {
 	command() {
 		const _this = this;
 
-		_this.api.getAlertText(value => {
-			if (value.status === 0) {
-				_this.api.acceptAlert();
+		_this.api.alerts.getText(value => {
+			const { error, status } = value;
+			const { message } = error || {};
+
+			if (status === 0) {
+				_this.api.alerts.accept();
+			} else if (message && message.includes("no such alert")) {
+				console.log("[INFO] Loadero: No alert is open.");
 			}
 
 			_this.emit("complete");
